98ad5519ff3ee4b76d0f87e115a8e5df44a6f465
Risha Surana <surana.risha@gmail.com>
2024-03-01 16:46:41 -0800

    stretch

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -24,9 +24,11 @@ def main(sector: str, queueName: str) -> None:
     #                       WRITE CODE HERE!!!
     #
     
-    bindingKey = f"#.{sector}.#"
-    consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
-    consumer.startConsuming()
+    sectors = sector.split(',')
+    for sector in sectors:
+        bindingKey = f"#.{sector}.#"
+        consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)   
+        consumer.startConsuming()
     
 
 if __name__ == "__main__":
